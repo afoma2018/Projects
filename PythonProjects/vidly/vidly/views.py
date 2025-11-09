eee2d014f9f610ab64eba7e4867ee6fec3340e9e
Tilaye Ayele <tatnunu@yahoo.com>
2025-11-09 16:07:26 -0600

    adding comments on the methodsd

--- PythonProjects/vidly/vidly/views.py
+++ PythonProjects/vidly/vidly/views.py
@@ -1,6 +1,18 @@
+"""_summary_
 
+Returns:
+    _type_: _description_
+"""
 from django.shortcuts import render
 
 
 def home(request):
+    """_summary_
+
+    Args:
+        request (_type_): _description_
+
+    Returns:
+        _type_: _description_
+    """
     return render(request, 'home.html')
